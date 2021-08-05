883b27d109caa3d100f050a631db32bd4ff0abcd
Dilekcn <dcanturk1@gmail.com>
2021-08-05 23:02:48 +0100

    A - list -of -names completed

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,14 @@
+
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  let h1 = document.createElement("h1")
+  let h2 = document.createElement("h2")
+  content.append(h1)
+  content.append(h2)
+  for(let i =0 ; i<arrayOfPeople.length; i++){
+h1.innerHTML += arrayOfPeople[i].name + " ";
+h2.innerHTML += arrayOfPeople[i].job + " ";
+  }
 }
 
 let people = [
@@ -9,3 +18,12 @@ let people = [
 ];
 
 listOfNames(people);
+
+
+/**
+ <div id="content">
+  <h1>{Name Here}</h1>
+  <h2>{Job Here}</h2>
+  .....
+</div>
+ */
