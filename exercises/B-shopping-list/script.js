9a422c2f13c4c1c38c381b840835840e47e5ad51
Dilekcn <dcanturk1@gmail.com>
2021-08-06 00:43:51 +0100

    B sopping list completed

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,21 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let ul = document.createElement("ul")
+  content.appendChild(ul);
+
+  arrayOfPeople.forEach(element =>{
+  let li = document.createElement("li");
+      li.innerHTML = element;
+      ul.appendChild(li);
+  });
+
+  // for(let i =0 ; i<arrayOfPeople.length; i++){
+  //  li.innerHTML = arrayOfPeople[i] ;
+  //     }
+  
+console.log(list)
+
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
