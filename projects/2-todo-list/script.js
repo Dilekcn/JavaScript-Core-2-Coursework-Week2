5c47f7a58d736af0ab7ddf08889bcf1a890a8d75
Dilekcn <dcanturk1@gmail.com>
2021-08-14 10:21:52 +0100

    2 todo list

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,6 +1,36 @@
 function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
+  // let list = document.getElementById("todo-list");
+  let list =document.querySelector("#todo-list")
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+console.log(list);
+todos.forEach(addTodo);
+
+}
+
+function addTodo(todo){
+  let list =document.querySelector("#todo-list");
+
+  console.log(todo.task);
+
+  let li = document.createElement("li");
+  
+  li.innerText = todo.task;
+  list.appendChild(li); 
+  li.className= "list-group-item d-flex justify-content-between align-items-center"
+
+  let span = document.createElement("span");
+  li.appendChild(span)
+  span.className = "badge bg-primary rounded-pill";
+
+  let check = document.createElement("i");
+  check.innerHTML=`<i class="fa fa-check" aria-hidden="true"></i>`
+  span.appendChild(check);
+
+  let trash = document.createElement("i");
+  // trash.className ="fa fa-trash";
+  span.appendChild(trash)
+  trash.innerHTML =` <i class="fa fa-trash" aria-hidden="true"></i>`
+
 }
 
 // These are the same todos that currently display in the HTML
@@ -14,8 +44,10 @@ populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
+    // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
+  
+  
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
 }
 
