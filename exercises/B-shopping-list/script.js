9c80004668c789d22cbdcf9b5e072845ec6d0940
Dilekcn <dcanturk1@gmail.com>
2021-09-04 22:16:04 +0100

    B done

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,16 @@
 function shoppingList(arrayOfPeople) {
+
+  let content = document.querySelector("#content");
+  let ol = document.createElement("ol");
+  content.appendChild(ol)
+  arrayOfPeople.forEach(item =>{
+  let li = document.createElement("li");
+  ol.appendChild(li)
+  li.innerText =item;
+  })
   // Write your code here...
 }
 
-let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
+let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea","Coffe"];
 
 shoppingList(shopping);
