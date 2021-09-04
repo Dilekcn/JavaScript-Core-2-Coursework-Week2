1ac2d8f13c186c9ae36812b195522a6e177fce87
Dilekcn <dcanturk1@gmail.com>
2021-09-04 23:58:31 +0100

    C done

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,4 +1,39 @@
 function readingList(books) {
+  let content = document.querySelector("#content");
+content.style.display ="flex";
+
+  for(let i=0; i<books.length; i++){
+    let card = document.createElement("div");
+    card.style.width ="340px";
+    card.style.backgroundColor ="yellow";
+    card.style.marginLeft ="20px";
+    card.style.paddingLeft ="20px";
+
+    content.appendChild(card)
+    let p = document.createElement("p");
+    card.appendChild(p)
+    p.innerText = books[i].title +" - "+ books[i].author
+
+    let img =document.createElement("img");
+    img.style.marginTop = "20px" 
+    img.style.width = "70%" 
+    img.style.height = "70%" 
+    img.src = books[i].img;
+    p.appendChild(img)
+
+    let isRead = document.createElement("p");
+    p.appendChild(isRead);
+    if(books[i].alreadyRead === true){
+      isRead.innerText ="You read this book!!"
+    }else{
+      isRead.innerText ="You do not read this book!!"
+      isRead.style.color="red"
+      isRead.style.fontSize= "16px"
+
+    }
+
+  }
+
   // Write your code here...
 }
 
@@ -6,17 +41,20 @@ const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
-    alreadyRead: false
+    alreadyRead: false,
+    img:"https://pictures.abebooks.com/isbn/9780385267748-uk.jpg"
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true
+    alreadyRead: true,
+    img:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1295465264l/8884400.jpg"
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
-    alreadyRead: true
+    alreadyRead: true,
+    img:"https://m.media-amazon.com/images/I/51A8l+FxFNL.jpg"
   }
 ];
 
