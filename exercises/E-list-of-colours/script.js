e8c295c7e39b5925927bf6845df4205a6b8624d8
Dilekcn <dcanturk1@gmail.com>
2021-09-05 02:25:00 +0100

    E done

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,4 +1,23 @@
 function listOfColours(colours) {
+  console.log(colours)
+  let content = document.getElementById("content");
+  let select = document.createElement("select");
+  content.appendChild(select)
+  let p = document.createElement("p");
+  content.appendChild(p)
+
+
+  colours.forEach(color =>{
+    let option = document.createElement("option");
+    option.innerText = color;
+    select.appendChild(option)
+
+  })
+select.addEventListener("change",function(){
+  p.style.color = select.value;
+  p.innerText = `You have selected: ${select.value}`
+})
+
   // Write your code here...
 }
 
